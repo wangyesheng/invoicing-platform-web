90c8eaf142315370a2a853df6aea91d17cf872a8
wangyesheng <1270230529@qq.com>
2021-01-03 23:24:41 +0800

    feat: 融入动态表单初版

--- src/config/ui.js
+++ src/config/ui.js
@@ -1,4 +1,6 @@
 import Vue from "vue";
 import EOSTag from "@/components/EOSTag";
+import EOSForm from "@/components/EOSForm";
 
 Vue.component("eos-tag", EOSTag);
+Vue.component("eos-form", EOSForm);
